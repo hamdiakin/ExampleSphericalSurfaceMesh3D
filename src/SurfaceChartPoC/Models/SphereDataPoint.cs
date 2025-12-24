eaa5ff5125ff0ba9b1a47886cad7cddde55243cb
Hamdi Akın <hamdiakin@Hamdis-MacBook-Pro.local>
2025-12-24 12:10:56 +0300

    Cleanup legacy PoC projects and enable split libraries

--- src/SurfaceChartPoC/Models/SphereDataPoint.cs
+++
@@ -1,8 +0,0 @@
-// This file now serves as a type alias to maintain backward compatibility
-// The actual implementation is in Common.Domain.SphereDataPoint
-
-namespace SurfaceChartPoC.Models
-{
-    using SphereDataPoint = Common.Domain.SphereDataPoint;
-}
- 
